48aac3e9c4a21a2f77e4245b64c1d7f1c20ec193
victor.guidi <vsantosg4072@gmail.com>
2022-07-28 04:07:26 -0300

    Added a while loop and some booleand variables

--- guessGame/guess.cpp
+++ guessGame/guess.cpp
@@ -9,21 +9,37 @@ int main() {
 
   // Variables in c++ are static, so we must always pass the type, although we
   // do not have to give it a valeu:
-  int number = 50;
+  const int NUMBER = 50;
+  bool notGuessed = true;
+  int guesses = 0;
 
-  int guess;
+  // adding a loop so the game won't finsih until the player guesses correclty
+  while (notGuessed) {
+    guesses++;
+    int guess;
 
-  cout << "Guess a number: " << endl;
+    cout << "This guess will be the number: " << guesses << endl;
 
-  // In order to read or receive an output, we can use cin:
-  cin >> guess;
+    cout << "Guess a number: " << endl;
 
-  // There are three main logical operators, if, else if and else
-  if (guess == number) {
-    cout << "Your guess is correct!!" << endl;
-  } else if (guess > number) {
-    cout << "Your guess is bigger then the number" << endl;
-  } else {
-    cout << "Your guess is lower then the number" << endl;
+    // In order to read or receive an output, we can use cin:
+    cin >> guess;
+
+    // Declare two boolean variables so it works as a condition for our
+    // validation
+    bool correct = guess == NUMBER;
+    bool higher = guess > NUMBER;
+
+    // There are three main logical operators, if, else if and else
+    if (correct) {
+      cout << "Your guess is correct!!" << endl;
+      notGuessed = false;
+    } else if (higher) {
+      cout << "Your guess is bigger then the number" << endl;
+    } else {
+      cout << "Your guess is lower then the number" << endl;
+    }
   }
+  cout << "Congrats, you finished the game in: " << guesses << " guesses!!"
+       << endl;
 }
