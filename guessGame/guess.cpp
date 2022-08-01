1ef55744a91c7be6df0ca44c062e1a8b201f1c02
victor.guidi <vsantosg4072@gmail.com>
2022-08-01 04:23:08 -0300

    Finished the first little game!!

--- guessGame/guess.cpp
+++ guessGame/guess.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 int main() {
@@ -7,18 +8,46 @@ int main() {
   // with endl
   cout << "Welcome to my Game!!" << endl;
 
+  cout << "Choose the difficulty" << endl;
+  cout << "Easy(E), Medium(M) and Hard(H)" << endl;
+
+  // Creating levels of difficulty
+  char difficulty;
+  int trys = 0;
+
+  cin >> difficulty;
+
+  if (difficulty == 'E') {
+    trys = 15;
+  } else if (difficulty == 'M') {
+    trys = 10;
+  } else {
+    trys = 5;
+  }
+
   // Variables in c++ are static, so we must always pass the type, although we
-  // do not have to give it a valeu:
-  const int NUMBER = 50;
+  // do not have to give it a value:
+
+  // The number that the player will have to find:
+  // const int NUMBER = 50;
+
+  // First it was a constant, but now we are randomly generating a new number
+  // based on time
+  srand(time(0));
+  int number = rand() % 100;
+  cout << number << endl;
+
   bool notGuessed = true;
   int guesses = 0;
 
   // adding a points system
   double score = 1000;
 
+  cout << "The game has started, you have " << trys << " trys to guess" << endl;
+
   // adding a loop so the game won't finsih until the player guesses correclty
-  while (notGuessed) {
-    guesses++;
+  // or the ammount of trys are over
+  for (guesses = 1; guesses <= trys; guesses++) {
     int guess;
 
     cout << "This guess will be the number: " << guesses << endl;
@@ -30,18 +59,19 @@ int main() {
 
     // We can calculate the points already here becaue it is not related to the
     // fact if was correct or not
-    double lost_points = abs(guess - NUMBER) / 2.0;
+    double lost_points = abs(guess - number) / 2.0;
     score = score - lost_points;
 
     // Declare two boolean variables so it works as a condition for our
     // validation
-    bool correct = guess == NUMBER;
-    bool higher = guess > NUMBER;
+    bool correct = guess == number;
+    bool higher = guess > number;
 
     // There are three main logical operators, if, else if and else
     if (correct) {
       cout << "Your guess is correct!!" << endl;
       notGuessed = false;
+      break;
     } else if (higher) {
       cout << "Your guess is bigger then the number" << endl;
     } else {
@@ -52,7 +82,12 @@ int main() {
   // adding 2 points of precision for our final score:
   cout.precision(2);
   cout << fixed;
-  cout << "Congrats, you finished the game in: " << guesses << " guesses!!"
-       << endl;
-  cout << "Your score was: " << score << endl;
+
+  if (notGuessed) {
+    cout << "You lost the game" << endl;
+  } else {
+    cout << "Congrats, you finished the game in: " << guesses - 1
+         << " guesses!!" << endl;
+    cout << "Your score was: " << score << endl;
+  }
 }
