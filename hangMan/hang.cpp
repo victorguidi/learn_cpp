229b6c2cb541dea71ee4a8926b4fa69ec4e38874
victor.guidi <vsantosg4072@gmail.com>
2022-08-03 05:17:16 -0300

    The game is already playable!!!

--- hangMan/hang.cpp
+++ hangMan/hang.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <map>
 #include <string>
+#include <vector>
 using namespace std;
 
 bool checkLetter(string word, char letter) {
@@ -21,22 +23,64 @@ bool checkLetter(string word, char letter) {
   return false;
 }
 
+bool not_guessed(string word, map<char, bool> hide) {
+  for (char l : word) {
+    if (!hide[l]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool not_hanged(vector<char> wrong_guesses) { return wrong_guesses.size() < 5; }
+
 int main() {
   string word = "guess";
-  cout << word << endl;
+
+  // Using maps is c++ is just like using a dict
+  map<char, bool> hide;
+
+  // In order to create dynamic arrays in c++ we can use vectors:
+  vector<char> wrong_guesses;
 
   // boolean variables that will help us to create the condition for the loop
-  bool not_guessed = true;
-  bool not_hanged = true;
 
-  while (not_guessed && not_hanged) {
+  while (not_guessed(word, hide) && not_hanged(wrong_guesses)) {
+
+    cout << "You already guessed this words: ";
+
+    for (char l : wrong_guesses) {
+      cout << l << " ";
+    }
+    cout << endl;
+
+    for (char l : word) {
+      if (hide[l]) {
+        cout << l;
+      } else {
+        cout << "_ ";
+      }
+    }
+    cout << endl;
+
     char guess;
     cin >> guess;
+
+    hide[guess] = true;
 
     if (checkLetter(word, guess)) {
       cout << "This letter exists in the word" << endl;
     } else {
       cout << "This letter does not exists in the word" << endl;
+      wrong_guesses.push_back(guess);
     }
+
+    cout << endl;
+  }
+
+  if (not_hanged(wrong_guesses)) {
+    cout << "Congrats! You won the game, and the word was: " << word << endl;
+  } else {
+    cout << "You lost the game! The word was: " << word << endl;
   }
 }
